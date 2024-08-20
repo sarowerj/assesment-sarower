3171e0b991bc111a892098f2531434313d7788d6
@sharower <sharower.jahan@eongroup.com.bd>
2024-08-20 12:25:13 +0600

    Initial data added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,85 +1,89 @@
-import {  StyleSheet, Modal } from 'react-native';
+import { StyleSheet, Modal } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { useNavigation } from 'expo-router';
-import { Container } from '@/components/Container';
-import { CategoryBox } from '@/components/CategoryBox';
-import { BookmarkType, BookmarkTypeGroup } from '../../types/Bookmark';
-import { Button } from '@/components/Button';
-import { ModalBody } from '@/components/ModalBody';
-import { AddBookMark } from '@/components/forms/AddBookmark';
-import { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useNavigation } from "expo-router";
+import { Container } from "@/components/Container";
+import { CategoryBox } from "@/components/CategoryBox";
+import { BookmarkType, BookmarkTypeGroup } from "../../types/Bookmark";
+import { Button } from "@/components/Button";
+import { ModalBody } from "@/components/ModalBody";
+import { AddBookMark } from "@/components/forms/AddBookmark";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function HomeScreen() {
-  const navigation = useNavigation()
-  const[bookmarkModal, setBookmarkModal] = useState(false)
+  const navigation = useNavigation();
+  const [bookmarkModal, setBookmarkModal] = useState(false);
   const [bookmarks, setBookmarks] = useState<BookmarkTypeGroup[]>();
 
-
-  const data:BookmarkType[] = [
+  const data: BookmarkType[] = [
     {
-      id:1,
-      title:'Javascript',
-      url:'https://www.javascript.com/',
-      category:'A'
+      id: 1,
+      title: "Javascript",
+      url: "https://www.javascript.com/",
+      category: "A",
     },
     {
-      id:2,
-      title:'HTML',
-      url:'https://html.com/',
-      category:'A'
+      id: 2,
+      title: "HTML",
+      url: "https://html.com/",
+      category: "A",
     },
-  ]
+  ];
 
-  const data2:BookmarkTypeGroup[] = [
+  const data2: BookmarkTypeGroup[] = [
     {
-      category:'A',
-      items:data
+      category: "A",
+      items: data,
     },
-  ]
-  
-  useEffect(()=>{
-    
-  async function getAllBookmarks() {
-    // await AsyncStorage.setItem('bookmarks', JSON.stringify(data2))
-    const allKeys = await AsyncStorage.getItem('bookmarks');
-    if(allKeys){
-      setBookmarks(JSON.parse(allKeys))
-    }
-  }
+  ];
 
-  getAllBookmarks()
-  },[])
-
-  const updateBookmarks = () => {
-    setBookmarkModal(false)
+  useEffect(() => {
     async function getAllBookmarks() {
-      // await AsyncStorage.setItem('bookmarks', JSON.stringify(data2))
-      const allKeys = await AsyncStorage.getItem('bookmarks');
-      if(allKeys){
-        setBookmarks(JSON.parse(allKeys))
+      await AsyncStorage.setItem("bookmarks", JSON.stringify(data2));
+      const allKeys = await AsyncStorage.getItem("bookmarks");
+      if (allKeys) {
+        setBookmarks(JSON.parse(allKeys));
       }
     }
-  
-    getAllBookmarks()
 
-  }
+    getAllBookmarks();
+  }, []);
+
+  const updateBookmarks = () => {
+    setBookmarkModal(false);
+    async function getAllBookmarks() {
+      // await AsyncStorage.setItem('bookmarks', JSON.stringify(data2))
+      const allKeys = await AsyncStorage.getItem("bookmarks");
+      if (allKeys) {
+        setBookmarks(JSON.parse(allKeys));
+      }
+    }
+
+    getAllBookmarks();
+  };
 
   //shrc@selisegroup.com
   return (
     <Container>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="subtitle">Bookmark Manager</ThemedText>
-        <Button title='Add Bookmark' onPress={()=>setBookmarkModal(true)} />
+        <Button title="Add Bookmark" onPress={() => setBookmarkModal(true)} />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        {bookmarks?<CategoryBox data={bookmarks}></CategoryBox>:<ThemedText>There is no bookmarks</ThemedText>}
+        {bookmarks ? (
+          <CategoryBox data={bookmarks}></CategoryBox>
+        ) : (
+          <ThemedText>There is no bookmarks</ThemedText>
+        )}
       </ThemedView>
       <Modal transparent={true} visible={bookmarkModal}>
         <ModalBody>
-          <AddBookMark handleSave={()=>updateBookmarks()} handleCancel={()=>setBookmarkModal(false)} />
+          <AddBookMark
+            handleSave={() => updateBookmarks()}
+            handleCancel={() => setBookmarkModal(false)}
+          />
         </ModalBody>
       </Modal>
     </Container>
@@ -88,10 +92,10 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
-    paddingVertical:20
+    paddingVertical: 20,
   },
   stepContainer: {
     gap: 8,
@@ -102,6 +106,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
